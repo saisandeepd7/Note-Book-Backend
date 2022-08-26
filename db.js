afbfcdfc8d1f51ca5825710048e3991fce2395d5
saisandeepd7 <dsandeep3690@gmail.com>
2022-08-26 14:26:12 +0530

    notebook

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4"
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
